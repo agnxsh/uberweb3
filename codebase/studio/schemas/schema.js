48892dc52fc8f54ba533026e834a625ab78de29e
agnxsh <iamagnix@gmail.com>
2022-03-25 01:48:32 +0530

    backend started

--- codebase/studio/schemas/schema.js
+++ codebase/studio/schemas/schema.js
@@ -1,10 +1,12 @@
 import createSchema from "part:@sanity/base/schema-creator";
 
 import schemaTypes from "all:part:@sanity/base/schema-type";
-
+import { userSchema } from "./userSchema";
+import { tripSchema } from "./tripSchema";
+import { ridesSchema } from "./ridesSchema";
 export default createSchema({
   // We name our schema
   name: "default",
 
-  types: schemaTypes.concat([]),
+  types: schemaTypes.concat([userSchema, tripSchema, ridesSchema]),
 });
